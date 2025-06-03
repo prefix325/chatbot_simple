3f24085fc1e9976c3921d75f2a8b76cac11fcf14
r-curiel <academico.curiel@gmail.com>
2025-06-03 19:58:11 -0300

    Primeiro commit

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -1,9 +1,47 @@
+// import {
+//   customProvider,
+//   extractReasoningMiddleware,
+//   wrapLanguageModel,
+// } from 'ai';
+import { xai } from '@ai-sdk/xai';
+// import { isTestEnvironment } from '../constants';
+// import {
+//   artifactModel,
+//   chatModel,
+//   reasoningModel,
+//   titleModel,
+// } from './models.test';
+
+// export const myProvider = isTestEnvironment
+//   ? customProvider({
+//       languageModels: {
+//         'chat-model': chatModel,
+//         'chat-model-reasoning': reasoningModel,
+//         'title-model': titleModel,
+//         'artifact-model': artifactModel,
+//       },
+//     })
+//   : customProvider({
+//       languageModels: {
+//         'chat-model': xai('grok-2-1212'),
+//         'chat-model-reasoning': wrapLanguageModel({
+//           model: xai('grok-3-mini-beta'),
+//           middleware: extractReasoningMiddleware({ tagName: 'think' }),
+//         }),
+//         'title-model': xai('grok-2-1212'),
+//         'artifact-model': xai('grok-2-1212'),
+//       },
+//       imageModels: {
+//         'small-model': xai.image('grok-2-image'),
+//       },
+//     });
+
 import {
   customProvider,
   extractReasoningMiddleware,
   wrapLanguageModel,
 } from 'ai';
-import { xai } from '@ai-sdk/xai';
+import { google } from '@ai-sdk/google';
 import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
@@ -23,13 +61,13 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-1212'),
+        'chat-model': google('gemini-pro'),
         'chat-model-reasoning': wrapLanguageModel({
-          model: xai('grok-3-mini-beta'),
+          model: google('gemini-pro'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
+        'title-model': google('gemini-pro'),
+        'artifact-model': google('gemini-pro'),
       },
       imageModels: {
         'small-model': xai.image('grok-2-image'),
